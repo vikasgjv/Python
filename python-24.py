b3bff27dcfdab875fa93c8544be9b137be05969b
Vikas G J <vikasgjv@gmail.com>
2026-07-19 11:40:08 +0530

    tuples

---
+++ python-24.py
@@ -0,0 +1,15 @@
+# tuples
+
+tup = (1,2,3,4,4,5,'python')
+print(type(tup) , tup)
+
+print(tup[0])
+print(len(tup))
+print(tup[-2])
+
+if 3 in tup:
+    print("yes")
+
+#slicing
+print(tup[1:3]) 
+print(tup)
