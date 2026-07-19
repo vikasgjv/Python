3906b51f9dd825b67e72f9cd58d0acf5c8f9ae9e
Vikas G J <vikasgjv@gmail.com>
2026-07-19 21:22:39 +0530

    Docstrings & PEP 8

---
+++ python-29.py
@@ -0,0 +1,8 @@
+def square (n):
+    '''it takes n and prints square of the n'''              #it shld be right after def
+    return n**2
+n=5
+print(square(n))
+print(square.__doc__) #it prints the docstring (comments) ''' ''' considered as docstring right after the def
+
+import this #read the printed peom to understands
