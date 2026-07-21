6d05840e114b2e1107071474c01117034a372706
Vikas G J <vikasgjv@gmail.com>
2026-07-21 11:28:31 +0530

    if __name__ == __main__

---
+++ python-45.py
@@ -0,0 +1,10 @@
+#  if __name__ == "__main__"
+
+def welcome():
+    print("Hii guys...")
+
+if __name__ == "__main__":
+    welcome()
+
+#without this name and main ...whn we import 
+# an unknown module it can excute all the print statements by using this we can prevent it
--- vikas.py
+++ vikas.py
@@ -1,4 +1,4 @@
 def welcome():
     print("Hii guys...")
 
-welcome()
+ 
