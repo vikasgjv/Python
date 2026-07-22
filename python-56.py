87f9511bf1b314a3aa0713271ba3b2dd34562b05
Vikas G J <vikasgjv@gmail.com>
2026-07-22 19:36:36 +0530

    Introduction to OOPs

---
+++ python-56.py
@@ -0,0 +1,20 @@
+# Introduction to OOPs 
+'''\Introduction to OOPs  :  mainly there are two approaches write a code or program
+							1) procedural programming - wat we are following till now
+							2) object-oriented programming - is to use classes and objects to represent real-world concepts and entites
+							
+OOPs:
+	class : it is a blueprint or a template for creating objects.
+	object : instance of a class,it contains it won data and methods
+	
+	key features:
+				encapsulation : internal state of an object is hidden and can only be accessed or modified through the object's method.
+				inheritance	: 	which allows new classes to be created that inherit the properites and method of an exsisting class.'''
+
+'''
+railway form - class [blueprint]
+        jon - jon info form [object / entity]
+        walter - walter info form [object / entity]
+
+vip railwayform- creatd using exsisting railway form its called inhertiance.
+'''
