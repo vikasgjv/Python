b3264b49a1eebbe75f79edfa0760d3b2156d63e1
Vikas G J <vikasgjv@gmail.com>
2026-08-03 13:24:51 +0530

    Getters & Setters

---
+++ python-60.py
@@ -0,0 +1,25 @@
+# Getters & Setters
+
+class myclass:
+    def __init__ (self,value):
+        self.value2 = value
+
+    def show(self):
+        print(f"The value is {self.value2}")
+   
+    @property               #Getter
+    def ten_value(self):
+        return 10*self.value2
+
+    @ten_value.setter
+    def ten_value(self, new_value):
+        self.value2 = new_value /10
+
+
+
+obj = myclass(10)
+obj.ten_value = 69
+print(obj.ten_value)
+obj.show()
+
+ 
