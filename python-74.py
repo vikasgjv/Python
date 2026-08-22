a24dae0c8ca6aa89a415e9f6ceda946d87f9f167
Vikas G J <vikasgjv@gmail.com>
2026-08-22 12:30:49 +0530

    Method Overriding

---
+++ python-74.py
@@ -0,0 +1,22 @@
+# Method Overriding 
+
+class shape:
+    def __init__(self , x ,y):
+        self.x = x
+        self.y = y
+
+    def area(self):
+        return self.x * self.y
+    
+class circle(shape):
+    def __init__(self,radius):
+        self.radius = radius
+        super().__init__(radius,radius)
+
+    def area(self):
+        return 3.14 * super().area()
+    
+c = circle(5)
+print(c.area())
+
+
