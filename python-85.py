cffcd24b252de4decf6c5b33e6547ba70a287821
Vikas G J <vikasgjv@gmail.com>
2026-08-24 11:38:18 +0530

    Creating command line utility

---
+++ python-85.py
@@ -0,0 +1,8 @@
+'''
+Creating command line utility : command line utilites are programs that can be run from the terminal or commandline interface and they 
+									are an essential part of many development workflows .
+								- In python you can create your own commandline utilites using the built-in argparse module.
+'''
+
+import argparse
+parser = argparse.ArgumentParser()
