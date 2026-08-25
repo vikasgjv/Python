c76e9e5f2348f486345f76528409fb5894bd84dc
Vikas G J <vikasgjv@gmail.com>
2026-08-25 09:53:38 +0530

    # Multithreading

---
+++ python-97.py
@@ -0,0 +1,24 @@
+# Multithreading
+
+
+import time
+import threading
+
+def func(seconds):
+    print(f"Sleeping for {seconds} seconds")
+    time.sleep(seconds)
+
+# Normal Code
+func(4)
+func(2)
+func(1)
+
+# Same code using Threads
+t1 = threading.Thread(target=func, args=[4])
+t2 = threading.Thread(target=func, args=[2])
+t3 = threading.Thread(target=func, args=[1])
+
+t1.start()
+t2.start()
+t3.start()
+ 
